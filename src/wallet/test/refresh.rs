3434400a47d0a8ac5ece06ab22f904f6e5771977
Nicola Busanello <nicola.busanello@gmail.com>
2024-10-14 16:23:53 +0200

    add refresh::skip_sync test

--- src/wallet/test/refresh.rs
+++ src/wallet/test/refresh.rs
@@ -593,3 +593,288 @@ fn uda_with_preview_and_reserves() {
     assert_eq!(reserves.utxo.to_string(), FAKE_TXID);
     assert_eq!(reserves.proof, proof);
 }
+
+#[cfg(feature = "electrum")]
+#[test]
+#[parallel]
+fn skip_sync() {
+    initialize();
+
+    let amount_1: u64 = 66;
+    let amount_2: u64 = 33;
+
+    let filter_counter_in = RefreshFilter {
+        status: RefreshTransferStatus::WaitingCounterparty,
+        incoming: true,
+    };
+    let filter_counter_out = RefreshFilter {
+        status: RefreshTransferStatus::WaitingCounterparty,
+        incoming: false,
+    };
+    let filter_confirm_in = RefreshFilter {
+        status: RefreshTransferStatus::WaitingConfirmations,
+        incoming: true,
+    };
+    let filter_confirm_out = RefreshFilter {
+        status: RefreshTransferStatus::WaitingConfirmations,
+        incoming: false,
+    };
+
+    let (wallet_1, online_1) = get_funded_wallet!();
+    let (wallet_2, online_2) = get_funded_wallet!();
+
+    // issue
+    let asset_1 = test_issue_asset_nia(&wallet_1, &online_1, Some(&[AMOUNT, AMOUNT]));
+    let asset_2 = test_issue_asset_nia(&wallet_2, &online_2, Some(&[AMOUNT * 2, AMOUNT * 2]));
+
+    // per each wallet prepare:
+    // - 1 WaitingCounterparty + 1 WaitingConfirmations ountgoing
+    // - 1 WaitingCounterparty + 1 WaitingConfirmations incoming
+
+    stop_mining();
+
+    // wallet 1 > wallet 2 WaitingConfirmations and vice versa
+    let receive_data_2a = test_blind_receive(&wallet_2);
+    let recipient_map_1a = HashMap::from([(
+        asset_1.asset_id.clone(),
+        vec![Recipient {
+            amount: amount_1,
+            recipient_id: receive_data_2a.recipient_id.clone(),
+            witness_data: None,
+            transport_endpoints: TRANSPORT_ENDPOINTS.clone(),
+        }],
+    )]);
+    // refresh skipping sync > no transfer has changed so return false before and after syncing
+    assert!(!wallet_2
+        .refresh(online_2.clone(), None, vec![], true)
+        .unwrap()
+        .transfers_changed());
+    wallet_2.sync(online_2.clone()).unwrap();
+    assert!(!wallet_2
+        .refresh(online_2.clone(), None, vec![], true)
+        .unwrap()
+        .transfers_changed());
+    let txid_1a = test_send(&wallet_1, &online_1, &recipient_map_1a);
+    assert!(!txid_1a.is_empty());
+    let receive_data_1a = test_blind_receive(&wallet_1);
+    let recipient_map_2a = HashMap::from([(
+        asset_2.asset_id.clone(),
+        vec![Recipient {
+            amount: amount_2,
+            recipient_id: receive_data_1a.recipient_id.clone(),
+            witness_data: None,
+            transport_endpoints: TRANSPORT_ENDPOINTS.clone(),
+        }],
+    )]);
+    let txid_2a = test_send(&wallet_2, &online_2, &recipient_map_2a);
+    assert!(!txid_2a.is_empty());
+    // refresh skipping sync > transfers have changed so return true
+    assert!(wallet_1
+        .refresh(online_1.clone(), None, vec![], true)
+        .unwrap()
+        .transfers_changed());
+    assert!(wallet_2
+        .refresh(online_2.clone(), None, vec![], true)
+        .unwrap()
+        .transfers_changed());
+    assert!(wallet_1
+        .refresh(
+            online_1.clone(),
+            Some(asset_1.asset_id.to_string()),
+            vec![filter_counter_out.clone()],
+            true,
+        )
+        .unwrap()
+        .transfers_changed());
+
+    // wallet 1 > 2, WaitingCounterparty and vice versa
+    let receive_data_2b = test_blind_receive(&wallet_2);
+    let recipient_map_1b = HashMap::from([(
+        asset_1.asset_id,
+        vec![Recipient {
+            amount: amount_1,
+            recipient_id: receive_data_2b.recipient_id.clone(),
+            witness_data: None,
+            transport_endpoints: TRANSPORT_ENDPOINTS.clone(),
+        }],
+    )]);
+    let txid_1b = test_send(&wallet_1, &online_1, &recipient_map_1b);
+    assert!(!txid_1b.is_empty());
+    // wallet 2 > 1, WaitingCounterparty
+    let receive_data_1b = test_blind_receive(&wallet_1);
+    show_unspent_colorings(&wallet_1, "wallet 1 after blind 1b");
+    let recipient_map_2b = HashMap::from([(
+        asset_2.asset_id,
+        vec![Recipient {
+            amount: amount_2,
+            recipient_id: receive_data_1b.recipient_id.clone(),
+            witness_data: None,
+            transport_endpoints: TRANSPORT_ENDPOINTS.clone(),
+        }],
+    )]);
+    let txid_2b = test_send(&wallet_2, &online_2, &recipient_map_2b);
+    assert!(!txid_2b.is_empty());
+    show_unspent_colorings(&wallet_2, "wallet 2 after send 2b");
+    assert!(check_test_transfer_status_sender(
+        &wallet_1,
+        &txid_1a,
+        TransferStatus::WaitingConfirmations
+    ));
+    assert!(check_test_transfer_status_sender(
+        &wallet_1,
+        &txid_1b,
+        TransferStatus::WaitingCounterparty
+    ));
+    assert!(check_test_transfer_status_recipient(
+        &wallet_1,
+        &receive_data_1a.recipient_id,
+        TransferStatus::WaitingConfirmations
+    ));
+    assert!(check_test_transfer_status_recipient(
+        &wallet_1,
+        &receive_data_1b.recipient_id,
+        TransferStatus::WaitingCounterparty
+    ));
+    assert!(check_test_transfer_status_sender(
+        &wallet_2,
+        &txid_2a,
+        TransferStatus::WaitingConfirmations
+    ));
+    assert!(check_test_transfer_status_sender(
+        &wallet_2,
+        &txid_2b,
+        TransferStatus::WaitingCounterparty
+    ));
+    assert!(check_test_transfer_status_recipient(
+        &wallet_2,
+        &receive_data_2a.recipient_id,
+        TransferStatus::WaitingConfirmations
+    ));
+    assert!(check_test_transfer_status_recipient(
+        &wallet_2,
+        &receive_data_2b.recipient_id,
+        TransferStatus::WaitingCounterparty
+    ));
+
+    // refresh incoming WaitingCounterparty only (wallet 1), skipping sync
+    assert!(wallet_1
+        .refresh(online_1.clone(), None, vec![filter_counter_in], true)
+        .unwrap()
+        .transfers_changed());
+    show_unspent_colorings(
+        &wallet_1,
+        "wallet 1 after refresh incoming WaitingCounterparty",
+    );
+    assert!(check_test_transfer_status_sender(
+        &wallet_1,
+        &txid_1a,
+        TransferStatus::WaitingConfirmations
+    ));
+    assert!(check_test_transfer_status_sender(
+        &wallet_1,
+        &txid_1b,
+        TransferStatus::WaitingCounterparty
+    ));
+    assert!(check_test_transfer_status_recipient(
+        &wallet_1,
+        &receive_data_1a.recipient_id,
+        TransferStatus::WaitingConfirmations
+    ));
+    assert!(check_test_transfer_status_recipient(
+        &wallet_1,
+        &receive_data_1b.recipient_id,
+        TransferStatus::WaitingConfirmations
+    ));
+
+    // refresh outgoing WaitingCounterparty only (wallet 2), skipping sync
+    assert!(wallet_2
+        .refresh(online_2.clone(), None, vec![filter_counter_out], true)
+        .unwrap()
+        .transfers_changed());
+    show_unspent_colorings(
+        &wallet_2,
+        "wallet 2 after refresh outgoing WaitingCounterparty",
+    );
+    assert!(check_test_transfer_status_sender(
+        &wallet_2,
+        &txid_2a,
+        TransferStatus::WaitingConfirmations
+    ));
+    assert!(check_test_transfer_status_sender(
+        &wallet_2,
+        &txid_2b,
+        TransferStatus::WaitingConfirmations
+    ));
+    assert!(check_test_transfer_status_recipient(
+        &wallet_2,
+        &receive_data_2a.recipient_id,
+        TransferStatus::WaitingConfirmations
+    ));
+    assert!(check_test_transfer_status_recipient(
+        &wallet_2,
+        &receive_data_2b.recipient_id,
+        TransferStatus::WaitingCounterparty
+    ));
+
+    mine(false, true);
+
+    // refresh incoming WaitingConfirmations only (wallet 2), skipping sync
+    assert!(wallet_2
+        .refresh(online_2.clone(), None, vec![filter_confirm_in], true)
+        .unwrap()
+        .transfers_changed());
+    show_unspent_colorings(
+        &wallet_2,
+        "wallet 2 after refresh incoming WaitingConfirmations",
+    );
+    assert!(check_test_transfer_status_sender(
+        &wallet_2,
+        &txid_2a,
+        TransferStatus::WaitingConfirmations
+    ));
+    assert!(check_test_transfer_status_sender(
+        &wallet_2,
+        &txid_2b,
+        TransferStatus::WaitingConfirmations
+    ));
+    assert!(check_test_transfer_status_recipient(
+        &wallet_2,
+        &receive_data_2a.recipient_id,
+        TransferStatus::Settled
+    ));
+    assert!(check_test_transfer_status_recipient(
+        &wallet_2,
+        &receive_data_2b.recipient_id,
+        TransferStatus::WaitingCounterparty
+    ));
+
+    // refresh outgoing WaitingConfirmations only (wallet 1), skipping sync
+    assert!(wallet_1
+        .refresh(online_1.clone(), None, vec![filter_confirm_out], true)
+        .unwrap()
+        .transfers_changed());
+    show_unspent_colorings(
+        &wallet_1,
+        "wallet 1 after refresh outgoing WaitingConfirmations",
+    );
+    assert!(check_test_transfer_status_sender(
+        &wallet_1,
+        &txid_1a,
+        TransferStatus::Settled
+    ));
+    assert!(check_test_transfer_status_sender(
+        &wallet_1,
+        &txid_1b,
+        TransferStatus::WaitingCounterparty
+    ));
+    assert!(check_test_transfer_status_recipient(
+        &wallet_1,
+        &receive_data_1a.recipient_id,
+        TransferStatus::WaitingConfirmations
+    ));
+    assert!(check_test_transfer_status_recipient(
+        &wallet_1,
+        &receive_data_1b.recipient_id,
+        TransferStatus::WaitingConfirmations
+    ));
+}
