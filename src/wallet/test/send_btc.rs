11701f2666928f169c497271edb40d6fd542ef54
Nicola Busanello <nicola.busanello@gmail.com>
2024-10-14 16:46:27 +0200

    add send_btc::skip_sync test

--- src/wallet/test/send_btc.rs
+++ src/wallet/test/send_btc.rs
@@ -126,3 +126,55 @@ fn fail() {
     );
     assert!(matches!(result, Err(Error::InvalidFeeRate { details: m }) if m == FEE_MSG_LOW));
 }
+
+#[cfg(feature = "electrum")]
+#[test]
+#[parallel]
+fn skip_sync() {
+    initialize();
+
+    let amount: u64 = 1000;
+
+    // wallets
+    let (wallet, online) = get_empty_wallet!();
+    let (rcv_wallet, _rcv_online) = get_empty_wallet!();
+
+    // prepare 1 UTXO
+    fund_wallet(test_get_address(&wallet));
+    let unspents = test_list_unspents(&wallet, Some(&online), false);
+    assert_eq!(unspents.len(), 1);
+
+    // send a 1st time skipping sync (spending the only UTXO)
+    let txid = wallet
+        .send_btc(
+            online.clone(),
+            test_get_address(&rcv_wallet),
+            amount,
+            FEE_RATE,
+            true,
+        )
+        .unwrap();
+    assert!(!txid.is_empty());
+    // send a 2nd time skipping sync > FailedBroadcast
+    let result = wallet.send_btc(
+        online.clone(),
+        test_get_address(&rcv_wallet),
+        amount,
+        FEE_RATE,
+        true,
+    );
+    assert_matches!(result, Err(Error::FailedBroadcast { details: _ }));
+
+    // sync and retry the 2nd send skipping sync > change UTXO is now available
+    wallet.sync(online.clone()).unwrap();
+    let txid = wallet
+        .send_btc(
+            online.clone(),
+            test_get_address(&rcv_wallet),
+            amount,
+            FEE_RATE,
+            true,
+        )
+        .unwrap();
+    assert!(!txid.is_empty());
+}
