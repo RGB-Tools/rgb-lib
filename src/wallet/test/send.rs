1d461afeb182ddb8266f38ebc46cd57b1b433e19
Zoe Faltibà <zoefaltiba@gmail.com>
2026-04-17 12:13:46 +0200

    lint code with rust 1.95.0

--- src/wallet/test/send.rs
+++ src/wallet/test/send.rs
@@ -7391,8 +7391,8 @@ fn allocations() {
             .collect();
         // check input colorings
         let input_colorings: Vec<_> = coloring_map
-            .iter()
-            .flat_map(|(_, c)| c)
+            .values()
+            .flatten()
             .filter(|c| c.r#type == ColoringType::Input)
             .collect();
         if pending_xfer {
@@ -7436,8 +7436,8 @@ fn allocations() {
         }
         // check change colorings
         let change_colorings: Vec<_> = coloring_map
-            .iter()
-            .flat_map(|(_, c)| c)
+            .values()
+            .flatten()
             .filter(|c| c.r#type == ColoringType::Change)
             .collect();
         assert_eq!(change_colorings.len(), amounts_auto.len());
@@ -7733,8 +7733,8 @@ fn allocations() {
     let db_asset_transfers = wallet_2.database().iter_asset_transfers().unwrap();
     // check input colorings
     let input_colorings: Vec<_> = coloring_map
-        .iter()
-        .flat_map(|(_, c)| c)
+        .values()
+        .flatten()
         .filter(|c| c.r#type == ColoringType::Input)
         .collect();
     // - 4 colorings
@@ -7771,8 +7771,8 @@ fn allocations() {
     }
     // check change colorings
     let change_colorings: Vec<_> = coloring_map
-        .iter()
-        .flat_map(|(_, c)| c)
+        .values()
+        .flatten()
         .filter(|c| c.r#type == ColoringType::Change)
         .filter(|c| {
             db_batch_transfers
